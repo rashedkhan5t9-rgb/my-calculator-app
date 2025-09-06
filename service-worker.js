d8f5c02c2d04f1c09fcabde432b527a6fa8326e3
rashedkhan5t9-rgb <rashed.khan5t9@gmail.com>
2025-09-06 16:39:25 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'price-calculator-pwa-cache-v1';
+const urlsToCache = [
+  '/my-calculator-app/calculator.html',
+  '/my-calculator-app/manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+}
